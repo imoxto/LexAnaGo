2e7a7b34ebfcea3426de7a8afa9aea21a2c1195e
xImouto <mohammad.rafid.hamid@g.bracu.ac.bd>
2022-02-18 19:11:06 +0600

    Lab1 done

--- main.go
+++ main.go
@@ -6,27 +6,70 @@ import (
 	"strings"
 )
 
-// utility functions
-func printArrLines(arr []string) {
-	fmt.Println("-----Printing an array-----")
-	for _, v := range arr {
-		fmt.Println(v)
-	}
-	fmt.Println("--------------------------")
-}
-
+// returns the index of the first matching element in data array
+// returns -1 if not found
 func indexOf(element string, data []string) int {
 	for k, v := range data {
 		if element == v {
 			return k
 		}
 	}
-	return -1 //not found.
+	return -1
 }
 
 // constant slices
-var Keywords = []string{"int", "float", "double", "boolean", "string", "if", "else"}
-var Others = []rune{';', ' ', ',', '[', '{', '(', ')', '}', ']'}
+var Keywords = []string{"int", "float", "double", "boolean", "string", "if", "else", "else if", "while", "for", "do", "continue", "break", "return"}
+var Seperators = []string{";", " ", ",", "[", "{", "(", ")", "}", "]", "=", "<", ">", "+", "-", "*", "/", "%", "!", "|", "&"}
+var LogicalOperators = []string{"<", ">", "==", "!=", "<=", ">=", "||", "&&"}
+var MathOperators = []string{"+", "-", "*", "/", "%", "=", "+=", "-=", "++", "--"}
+var PairSeperators = []string{"+=", "-=", "++", "--", "==", "!=", "<=", ">=", "||", "&&", "//"}
+var Digits = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+// checker functions
+func isKeyword(s string) bool {
+	return indexOf(s, Keywords) != -1
+}
+func isSeperator(s string) bool {
+	return indexOf(s, Seperators) != -1
+}
+func isPairSeperator(s string) bool {
+	return indexOf(s, PairSeperators) != -1
+}
+func isLogicOp(s string) bool {
+	return indexOf(s, LogicalOperators) != -1
+}
+func isMathOp(s string) bool {
+	return indexOf(s, MathOperators) != -1
+}
+
+func isIdentifier(s string) bool {
+	if s == "" {
+		return false
+	}
+	c := s[0]
+	if c == '_' || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'A') {
+		return true
+	}
+	return false
+}
+
+func isNumber(s string) bool {
+	if s == "" {
+		return false
+	}
+	d := false
+	for _, v := range s {
+		if v == '.' {
+			if d {
+				return false
+			}
+			d = true
+		} else if v < '0' || v > '9' {
+			return false
+		}
+	}
+	return true
+}
 
 type LexAnalyzeResult struct {
 	Keywords         []string
@@ -38,7 +81,6 @@ type LexAnalyzeResult struct {
 }
 
 func (l *LexAnalyzeResult) Display() {
-	fmt.Println("------- Printing Lexical Analysis Result -------")
 	if len(l.Keywords) > 0 {
 		fmt.Printf("Keywords: %v \n", strings.Join(l.Keywords, ", "))
 	}
@@ -55,28 +97,94 @@ func (l *LexAnalyzeResult) Display() {
 		fmt.Printf("NumericalValues: %v \n", strings.Join(l.NumericalValues, ", "))
 	}
 	if len(l.Others) > 0 {
-		fmt.Printf("Others: %v \n", strings.Join(l.Others, ", "))
-	}
-	fmt.Println("------------------------------------------------")
-}
-
-func (l *LexAnalyzeResult) AnalyzeLine(s string) {
-	sArr := strings.Split(strings.Trim(s, " "), " ")
-	n := len(sArr)
-	for i, v := range sArr {
-		if i == 0 && indexOf(v, Keywords) != -1 && indexOf(v, l.Keywords) == -1 {
-			l.Keywords = append(l.Keywords, v)
-		} else if i == n {
-			fmt.Printf("%v = %v\n", i, n)
-		}
+		fmt.Printf("Others: %v \n", strings.Join(l.Others, ""))
 	}
 }
 
 // main Lex Analysis function
-func LexAnalyze1(arr []string) (LexAnalyzeResult, error) {
+func LexAnalyze(s string) (LexAnalyzeResult, error) {
+
+	s += " " // added space to seperate the last char or token
+
 	var res = LexAnalyzeResult{}
-	for _, v := range arr {
-		res.AnalyzeLine(v)
+
+	left, right, n := 0, 0, len(s)-1
+
+	if n < 0 {
+		return res, nil
+	}
+
+	for right <= n && left <= right {
+		rightChar := string(s[right])
+
+		if isSeperator(rightChar) && right == left && right+1 <= n && isPairSeperator(s[right:right+2]) {
+			// enters here on seeing a valid PairSeperator
+
+			testStr := s[right : right+2]
+
+			if isLogicOp(testStr) {
+				if indexOf(testStr, res.LogicalOperators) == -1 {
+					// nested if to avoid checking else if when this condition fails
+					res.LogicalOperators = append(res.LogicalOperators, testStr)
+				}
+			} else if isMathOp(testStr) && indexOf(testStr, res.MathOperators) == -1 {
+				res.MathOperators = append(res.MathOperators, testStr)
+			}
+
+			right += 2
+			left = right
+
+		} else if isSeperator(rightChar) && right == left {
+			// enters here on seeing a valid seperator
+
+			if isLogicOp(rightChar) {
+				if indexOf(rightChar, res.LogicalOperators) == -1 {
+					// nested if to avoid checking else if when this condition fails
+					res.LogicalOperators = append(res.LogicalOperators, rightChar)
+				}
+			} else if isMathOp(rightChar) {
+				if indexOf(rightChar, res.MathOperators) == -1 {
+					// nested if to avoid checking else if when this condition fails
+					res.MathOperators = append(res.MathOperators, rightChar)
+				}
+			} else if rightChar != " " && indexOf(rightChar, res.Others) == -1 {
+				res.Others = append(res.Others, rightChar)
+			}
+			right++
+			left = right
+
+		} else if isSeperator(rightChar) && right > left {
+			// enters here after each token has been seperated
+
+			testStr := s[left:right]
+			if testStr == "else" && right+3 <= n && s[left:right+3] == "else if" {
+				right += 3
+				testStr = s[left:right]
+			}
+
+			if isKeyword(testStr) {
+				if indexOf(testStr, res.Keywords) == -1 {
+					// nested if to avoid checking else if when this condition fails
+					res.Keywords = append(res.Keywords, testStr)
+				}
+			} else if isIdentifier(testStr) {
+				if indexOf(testStr, res.Identifiers) == -1 {
+					// nested if to avoid checking else if when this condition fails
+					res.Identifiers = append(res.Identifiers, testStr)
+				}
+			} else if isNumber(testStr) {
+				if indexOf(testStr, res.NumericalValues) == -1 {
+					// nested if to avoid checking else if when this condition fails
+					res.NumericalValues = append(res.NumericalValues, testStr)
+				}
+			}
+			left = right
+
+		} else {
+
+			right++
+
+		}
 	}
 	return res, nil
 }
@@ -90,8 +198,11 @@ func main() {
 		panic(err)
 	}
 
-	dataByLine := strings.Split(string(data), "\n")
-	res, err := LexAnalyze1(dataByLine)
+	dataInArray := strings.Split(string(data), "\n")
+	for i := 0; i < len(dataInArray); i++ {
+		dataInArray[i] = strings.Trim(dataInArray[i], " ")
+	}
+	res, err := LexAnalyze(strings.Join(dataInArray, " "))
 	if err != nil {
 		fmt.Println("Something went wrong while analysing the data")
 		panic(err)
